ca887e6393c7666eca7d76bdece2e23dd56dd4e9
hejzbh <hazimba123@gmail.com>
2023-07-08 12:38:32 +0200

    Prepare for work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+// Next
+import type { AppProps } from "next/app";
+import { Poppins } from "next/font/google";
+
+const poppinsFont = Poppins({
+  subsets: ["latin-ext"],
+  weight: ["400", "500", "700"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={poppinsFont?.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
