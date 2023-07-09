1873dd3e89aeb371a695118ed5675704e9bf24be
hejzbh <hazimba123@gmail.com>
2023-07-09 22:40:40 +0200

    midleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  // If user trying to
+  // If the user tries to navigate to the defined pages without an ID.
   if (
     request?.nextUrl?.pathname === "/edit-quizz" ||
     request?.nextUrl?.pathname === "/play-quizz"
